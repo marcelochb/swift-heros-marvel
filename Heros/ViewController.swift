87899706c9f3263370f951793c6528b5da3d03ae
Marcelo Block <marcelo@gtmx.com.br>
2020-06-05 19:38:50 -0300

    Initial Commit

---
+++ Heros/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Heros
+//
+//  Created by Marcelo Block Teixeira on 05/06/20.
+//  Copyright © 2020 Marcelo Block Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
