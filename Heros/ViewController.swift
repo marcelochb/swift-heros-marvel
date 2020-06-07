58a9a72f7736f9d010f11308e10cafdffb45c787
Marcelo Block <marcelochb@gmail.com>
2020-06-07 04:28:51 -0300

    initial(first-commit) Started Project Heros Marvel

--- Heros/ViewController.swift
+++ Heros/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfHeroi: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btBuscar(_ sender: Any) {
+    }
+    
 }
 
